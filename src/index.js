89e282fb4ffef68a27630ec408e20af0cb18d140
Rishu <shrivastava.rishu5@gmail.com>
2026-07-26 11:36:16 +0530

    Update DB connection

--- src/index.js
+++ src/index.js
@@ -9,13 +9,10 @@ app.use(express.json());
 
 main()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server listening on port ${process.env.PORT}`);
     });
   })
   .catch((err) => {
     console.error('Database connection failed:', err.message);
-    app.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT} without DB connection`);
-    });
   });
